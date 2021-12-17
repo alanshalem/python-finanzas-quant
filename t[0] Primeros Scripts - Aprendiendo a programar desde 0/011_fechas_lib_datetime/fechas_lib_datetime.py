a0839c5c2157d3f6fd60819b9b1e52076b1cfd2f
shalema <alanshalem@gmail.com>
2021-12-16 23:48:12 -0300

    solving exercises

--- t[0] Primeros Scripts - Aprendiendo a programar desde 0/011_fechas_lib_datetime/fechas_lib_datetime.py
+++ t[0] Primeros Scripts - Aprendiendo a programar desde 0/011_fechas_lib_datetime/fechas_lib_datetime.py
@@ -5,8 +5,8 @@
 import datetime as dt
 # LA LIBRERIA DATETIME TIENE LOS SIGUIENTES PAQUETES O SUBLIBRERIA: date - time - datetime - timedelta - timezone - tzinfo
 # IMPORTANDO SUB-PAQUETES
-from datetime import datetime as dt_dt
-from datetime import date as dt_date
+from datetime import datetime as dt_dt # datetime
+from datetime import date as dt_date # date
 # ESOS ALIAS (dt_dt - dt_date) NOS PERMITEN REFERIRNOS EN MODO MAS ABREVIADO A LOS SUBPAQUETES.
 # EN LUGAR DE datetime.datetime SOLO DIREMOS dt_dt
 # EN LUGAR DE datetime.date SOLO DIREMOS dt_date
---
+++ t[0] Primeros Scripts - Aprendiendo a programar desde 0/012_ejercicios/ejercicios.py
@@ -0,0 +1,26 @@
+#1- Hacer un script que devuelva en la variable año, el año actual.
+#--------------------#
+# Rta Ejercicio 1:
+#--------------------#
+import datetime as dt
+from datetime import datetime as dt_dt
+año = dt_dt.now().year
+print(año)
+
+#2- Escribir el codigo que asigne un dia y mes definidos en las primeras lineas en las variables dia y mes respectivamente, y que devuelva que dia de la semana cae esa fecha en el año actual.
+#--------------------#
+# Rta Ejercicio 2:
+#--------------------#
+import calendar as cal
+dia = 1
+mes = 1
+año = dt_dt.now().year
+print(cal.weekday(año, mes, dia))
+
+
+#3- Hacer un script que le informe al usuario cuantos dias faltan para terminar el año
+#--------------------#
+# Rta Ejercicio 3:
+#--------------------#
+
+#4- Hacer un script que le informe al usuario que fraccion de año queda para la expiracion de una opcion que vence el 18 de diciembre del corriente año (que funcione sea cual sea el año que se ejecute el codigo)
