487f70e7277a54cb6e2b979f1d91696654a02d6f
shalema <alanshalem@gmail.com>
2021-12-21 01:12:17 -0300

    Add ejercicios listas y dicionarios

---
+++ t[0] Primeros Scripts - Aprendiendo a programar desde 0/014_ejercicios/014_ejercicios.py
@@ -0,0 +1,25 @@
+# EJERCICIO 1
+# Dada la siguiente lista:
+# listado = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# 1 - Los numeros pares
+# 2 - Los valores del 4 hasta el final de la lista
+# 3 - Los valores del 5 hasta el 10
+# 4 - Los valores menores a 10 que sean pares
+# 5 - Los multiplos de 3 ordenados al reves
+# 6 - El promedio de todos los numeros (usando las funciones sum y len)
+
+# Dadas las siguientes listas:
+# lideres = ['GGAL', 'PAMP', 'YPFD', 'TECO2', 'EDN', 'LOMA', 'BBAR']
+# galpones = ['AGRO', 'FIPL', 'MIRG', 'GARO', 'LONG']
+# 7 - Elegir un galpon al azar y agregarlo a la lista de lideres al final
+# 8 - Elegir un galpon al azar y reemplazar a EDN por ese galpon
+# 9 - Elegir 3 galpones al azar y reemplazar una lider cualquiera por esos dos galpones
+
+# EJERCICIO 2
+# Dado el siguiente diccionario:
+# panel = {'ALUA': 29.3, 'BBAR': 120.5, 'BMA': 265.2, 'BYMA': 290, 'CEPU': 29, 'COME': 3, 'CRES': 40.7}
+# 10 - Reemplazar el precio de BBAR por un precio al azar que elija python que varie +/- $1 del precio que tiene (con precision de 0.01)
+# 11 - Reemplazar el precio de BBAR por un precio al azar que elija python que varie +/- 3% del precio que tiene (con precision de 0.01)
+# 12 - Elegir un ticker al azar y mostrarlo
+# 13 - Re-ordenar el diccionario al azar
+# 14 - Re-ordenar el diccionario alfabeticamente en forma inversa (de la Z a la A)
